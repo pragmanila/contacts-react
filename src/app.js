c6e0834a8f60b69bb082deaa3f9fe85b08af1a6e
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-02-28 02:39:33 +0800

    Remove initialState from component.

--- src/app.js
+++ src/app.js
@@ -2,37 +2,6 @@ import React from "react";
 
 import "./app.scss";
 
-const contactList = [
-  {
-    id: "quaxi71a",
-    firstName: "Aubrey",
-    lastName: "Terry",
-    mobileNumber: "09591736637",
-    emailAddress: "aubreyterry@gmail.com"
-  },
-  {
-    id: "cqc3tmbxc",
-    firstName: "Kara",
-    lastName: "Lawrence",
-    mobileNumber: "09693943668",
-    emailAddress: "karalawrence@gmail.com"
-  },
-  {
-    id: "6e3gtfyzs",
-    firstName: "Raquel",
-    lastName: "Wright",
-    mobileNumber: "09177324640",
-    emailAddress: "raquelwright@gmail.com"
-  },
-  {
-    id: "lpwx12ww",
-    firstName: "Lola",
-    lastName: "Garrett",
-    mobileNumber: "09453244072",
-    emailAddress: "lolagarrett@gmail.com"
-  },
-];
-
 const App = () => {
   return (
     <div className="contact-container">
