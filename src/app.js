350c38051e6613180cb56d970f585f2037b88380
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-02-28 04:38:39 +0800

    Hide table when contact form is shown

--- src/app.js
+++ src/app.js
@@ -72,6 +72,63 @@ const App = () => {
         />
       </div>
       <ContactForm mode={contactFormMode}/>
+      {
+        contactFormMode === "hidden" && (
+          <table>
+            <thead>
+              <tr className="head">
+                <th></th>
+                <th>
+                  FIRST NAME
+                </th>
+                <th>
+                  MIDDLE NAME
+                </th>
+                <th>
+                  LAST NAME
+                </th>
+                <th>
+                  MOBILE NUMBER
+                </th>
+                <th>
+                  EMAIL ADDRESS
+                </th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                contactList.map(contact => (
+                  <tr key={ contact.id }>
+                    <td>
+                      <input
+                        checked={ selectedContactList.includes( contact.id ) }
+                        onChange={ () => {}}
+                        type="checkbox"
+                      />
+                      <label onClick={ () => { selectContact(contact.id)}}></label>
+                    </td>
+                    <td>{ contact.firstName }</td>
+                    <td>
+                      {
+                        contact.middleName ?
+                        contact.middleName :
+                        <span style={{ opacity: 0.4 }}>N/A</span>
+                      }
+                    </td>
+                    <td>{ contact.lastName }</td>
+                    <td>{ contact.mobileNumber }</td>
+                    <td>{ contact.emailAddress }</td>
+                    <td>
+                      <button className="edit">Edit contact</button>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        )
+      }
       <table>
         <thead>
           <tr className="head">
