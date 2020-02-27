235008bc1407b693337cf0d01d17105c72dae8fc
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-02-28 04:55:14 +0800

    Add fields to contact form

--- src/app.js
+++ src/app.js
@@ -24,7 +24,28 @@ const ContactForm = ({ mode, formWidth: width }) => {
   return (
     mode !== "hidden" && (
       <div className="contact-form" style={{ width }}>
-
+        <form onSubmit={() => {}}>
+          <label>
+            First name
+            <input type="text"/>
+          </label>
+          <label>
+            Middle name
+            <input type="text"/>
+          </label>
+          <label>
+            Last name
+            <input type="text"/>
+          </label>
+          <label>
+            Mobile number
+            <input type="text"/>
+          </label>
+          <label>
+            Email address
+            <input type="text"/>
+          </label>
+        </form>
       </div>
     )
   );
