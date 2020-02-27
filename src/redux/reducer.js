c16fc7f59b75c79cf1e45b348870c7e7423be8be
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-02-28 02:39:54 +0800

    Add reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,38 @@
+const initialState = {
+  contactList: [
+    {
+      id: "quaxi71a",
+      firstName: "Aubrey",
+      lastName: "Terry",
+      mobileNumber: "09591736637",
+      emailAddress: "aubreyterry@gmail.com"
+    },
+    {
+      id: "cqc3tmbxc",
+      firstName: "Kara",
+      lastName: "Lawrence",
+      mobileNumber: "09693943668",
+      emailAddress: "karalawrence@gmail.com"
+    },
+    {
+      id: "6e3gtfyzs",
+      firstName: "Raquel",
+      lastName: "Wright",
+      mobileNumber: "09177324640",
+      emailAddress: "raquelwright@gmail.com"
+    },
+    {
+      id: "lpwx12ww",
+      firstName: "Lola",
+      lastName: "Garrett",
+      mobileNumber: "09453244072",
+      emailAddress: "lolagarrett@gmail.com"
+    }
+  ]
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
