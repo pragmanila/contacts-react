790fd92a9c2ffffeb8efe3bbda5daa0a71bab0c1
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-02-28 16:17:23 +0800

    Fix minor code formatting issues

--- src/component/contact-list/contact-list.js
+++ src/component/contact-list/contact-list.js
@@ -2,13 +2,13 @@ import React, { forwardRef } from "react";
 
 const ContactList = forwardRef((
 	{
-	contactList,
-	selectContact,
-	selectedContactList
+    contactList,
+    selectContact,
+    selectedContactList
   },
   ref
 ) => (
-	<table ref={ref}>
+	<table ref={ ref }>
     <thead>
       <tr className="head">
         <th></th>
@@ -36,18 +36,17 @@ const ContactList = forwardRef((
           <tr key={ contact.id }>
             <td>
               <input
-              checked={ selectedContactList.includes( contact.id ) }
-              onChange={ () => {}}
-              type="checkbox"
+                checked={ selectedContactList.includes(contact.id) }
+                type="checkbox"
               />
-              <label onClick={ () => { selectContact(contact.id)}}></label>
+              <label onClick={ () => { selectContact(contact.id) } }/>
             </td>
             <td>{ contact.firstName }</td>
             <td>
               {
-              contact.middleName ?
-              contact.middleName :
-              <span style={{ opacity: 0.4 }}>N/A</span>
+                contact.middleName ?
+                contact.middleName :
+                <span style={{ opacity: 0.4 }}>N/A</span>
               }
             </td>
             <td>{ contact.lastName }</td>
